5c5490a9107963375a9ea2ab31e6e1fc7c8ebecb
Timothy Searcy <Timothy_Searcy@intuit.com>
2019-09-16 09:51:34 -0700

    tims unique stuff

--- wallpaperChanger.sh
+++ wallpaperChanger.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl -Lo ~/Documents/wallPaperchanger/image.png "https://source.unsplash.com/random/2560x1600/"
+curl -Lo /Users/tsearcy1/repos/wallPaperchanger/image.png "https://source.unsplash.com/random/2560x1600/?water,nature"
 # Mavericks
-sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '~/Documents/wallPaperchanger/image.png'" && killall Dock
+#sqlite3 /Users/tsearcy1/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '/Users/tsearcy1/repos/wallPaperchanger/image.png'" && killall Dock
 # Below Mavericks
-# osascript -e 'tell application "Finder" to set desktop picture to POSIX file "~/Documents/wallPaperchanger/image.png"' && killall Dock
+osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/tsearcy1/repos/wallPaperchanger/image.png"' && killall Dock
